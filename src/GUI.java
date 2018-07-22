3b973684a740cb0fe89ebf7b3273f481225b328b
dragomir44 <dragomiretsky@hotmail.com>
2018-07-22 04:14:57 +0200

    Random quote generator added TODO Fix percentage bug

--- src/GUI.java
+++ src/GUI.java
@@ -4,7 +4,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Sergey on 21-Jul-18.
  */
@@ -16,17 +21,24 @@ public class GUI {
     private JLabel percentage;
     private JPanel quotepanel;
     private JLabel quote;
+    private JLabel remaining;
 
     //Set the hours of your working day
     private int StartOfDay = 01;
     private int EndOfDay = 05;
+    private String QuoteFilename = "Quotes.txt";
+    private List<String> quotes;
+
 
     public GUI() {
-
+        ReadQuotes(QuoteFilename);
+        int randomquote = ThreadLocalRandom.current().nextInt(0, 20);
+        percentage.setText("Workday over");
         new Thread(){
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
             public void run() {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
                 while(true){
+                    long millis = System.currentTimeMillis();
                     //Using SimpleDateFormat which is a better practise than Calendar
                     Date now = new Date();
                     String strDate = sdf.format(now);
@@ -35,14 +47,22 @@ public class GUI {
                     //Calculating percentage of working day
                     int hour = Integer.parseInt(strDate.substring(0,2));
                     int minute = Integer.parseInt(strDate.substring(3,5));
+
                     if (hour < EndOfDay & hour >= StartOfDay){
                         float min = CalculateMinutes(StartOfDay, EndOfDay);
+                        remaining.setText(min + "");
+                        //FORMULA HAS BUG. TODO
                         float percent = Math.round(minute/min*100);
                         percentage.setText((int)percent + "%");
-                    } else {
-                        percentage.setText("Workday over");
                     }
-                    quote.setText("q");
+                    quote.setText(quotes.get(randomquote));
+
+                    //Sleep code for 1 second to prevent CPU drainage
+                    try {
+                        Thread.sleep(1000 - millis % 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
 
             }
@@ -53,40 +73,28 @@ public class GUI {
         return ((end-begin) * 60);
     }
 
-    public static String ReadQuotes(String filename){
-        String quotes = null;
-        try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            StringBuilder sb = new StringBuilder();
+    public void ReadQuotes(String filename){
+        quotes = new ArrayList<String>();
+        try(BufferedReader br = new BufferedReader(new FileReader("src/" + filename))) {
             String line = br.readLine();
-
             while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
+                quotes.add(line);
                 line = br.readLine();
             }
-            return quotes = sb.toString();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return null;
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
     }
     public static void main(String[] args) {
-        String quotes = null;
-        ReadQuotes("Quotes.txt");
         JFrame frame = new JFrame("GUI");
-        frame.setContentPane(new GUI().percentage);
-        frame.setContentPane(new GUI().percentpanel);
-        frame.setContentPane(new GUI().quote);
-        frame.setContentPane(new GUI().quotepanel);
-        frame.setContentPane(new GUI().button1);
-        frame.setContentPane(new GUI().clock);
-        frame.setContentPane(new GUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        GUI g = new GUI();
+        frame.add(g.panel1);
         frame.pack();
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+
     }
 }
